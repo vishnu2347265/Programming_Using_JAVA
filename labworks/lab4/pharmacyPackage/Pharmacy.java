7cba2b89490d27803eaccb24cfe87a970d261f34
vishnu2347265 <141517436+vishnu2347265@users.noreply.github.com>
2023-11-29 00:29:26 +0530

    Update Pharmacy.java

--- labworks/lab4/pharmacyPackage/Pharmacy.java
+++ labworks/lab4/pharmacyPackage/Pharmacy.java
@@ -1,5 +1,68 @@
-package labworks.lab4.pharmacyPackage;
+package lab_4.pharmacyPackage;
 
-public class Pharmacy {
-    
+public class Pharmacy implements PriceCalculable {
+    String id;
+    String product_name;
+    private int quantity;
+    private double price;
+
+    // default constructor
+    public Pharmacy() {
+        this.id = "Unknown";
+        this.product_name = null;
+        this.quantity = 0;
+        this.price = 0.0;
+    }
+
+    // parameterized constructor
+    public Pharmacy(String id, String product_name, double price) {
+        this.id = id;
+        this.product_name = product_name;
+        this.price = price;
+    }
+
+    // parameterized constructor 2
+    public Pharmacy(String id, String product_name, int quantity, double price) {
+        this.id = id;
+        this.product_name = product_name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public void display() {
+        System.out.println("Medication ID : " + id);
+        System.out.println("Medicine Name : " + product_name);
+        System.out.println("Quantity : " + quantity);
+        System.out.println("Price : " + price);
+        calculateTotalPrice("Aspirin");
+        System.out.println("Price of 10 Items : $" + calculateTotalPrice(10));
+        System.out.println("Total Price: $" + calculateTotalPrice());
+    }
+
+    // method overloading
+    public void calculateTotalPrice(String product_name) {
+        System.out.println("Total price of medicine is shown below :");
+    }
+
+    @Override
+    public double calculateTotalPrice() {
+        return price * quantity;
+    }
+
+    @Override
+    public double calculateTotalPrice(int quantity) {
+        return price * quantity;
+    }
+
+    public static void main(String[] args) {
+        Pharmacy pharmacy = new Pharmacy();
+        System.out.println("Default Constructor");
+        pharmacy.display();
+        Pharmacy medication1 = new Pharmacy("D308", "Paracetomol", 20.0);
+        System.out.println("\nConstructor Overloading 1");
+        medication1.display();
+        Pharmacy medication2 = new Pharmacy("D327", "Aspirin", 100, 48.5);
+        System.out.println("\nConstructor Overloading 2");
+        medication2.display();
+    }
 }
