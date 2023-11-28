b8f3c4b3ace690bc6f438e9e97269bee1f7d7f46
vishnu2347265 <141517436+vishnu2347265@users.noreply.github.com>
2023-11-29 00:30:42 +0530

    Main

---
+++ labworks/lab4/Main.java
@@ -0,0 +1,17 @@
+package lab_4;
+import lab_4.pharmacyPackage.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Pharmacy pharmacy = new Pharmacy();
+        System.out.println("Default Constructor");
+        pharmacy.display();
+        Pharmacy medication1 = new Pharmacy("D308", "Paracetomol", 20.0);
+        System.out.println("\nConstructor Overloading 1");
+        medication1.display();
+        Pharmacy medication2 = new Pharmacy("D327", "Aspirin", 100, 48.5);
+        System.out.println("\nConstructor Overloading 2");
+        medication2.display();
+    }
+}
